a9dab5790f46311f44a614fc736313f76ed05776
Vladimir Baranov <vladimir.m.baranov@gmail.com>
2026-09-10 10:48:55 -0400

    Add project files.

---
+++ CLiveCapture/CLiveCapture.cpp
@@ -0,0 +1,12 @@
+﻿// CLiveCapture.cpp : Defines the entry point for the application.
+//
+
+#include "CLiveCapture.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
